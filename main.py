2a212ced9a093c9d07796111dd91c6b50f9c709e
Rickard Hultgren <sababa.sababa@gmail.com>
2018-01-03 13:06:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import kivy
-#from datetime import datetime, timedelta
-
 kivy.require('1.7.2') # replace with your current kivy version !
-
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ListProperty, ObjectProperty, StringProperty, NumericProperty
@@ -11,31 +8,28 @@ from kivy.uix.button import Button
 from kivy.uix.tabbedpanel import TabbedPanel, TabbedPanelItem
 from kivy.uix.spinner import Spinner
 from kivy.uix.dropdown import DropDown
-#from kivy.uix.gridlayout import GridLayout
-#from functools import partial
 from kivy.lang import Builder
 from kivy.uix.popup import Popup
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
+from kivy.clock import Clock
+from kivy.uix.progressbar import ProgressBar
+from kivy.storage.jsonstore import JsonStore
+#from kivy.uix.gridlayout import GridLayout
+#from functools import partial
 #from kivy.uix.treeview import TreeView, TreeViewNode
 #from kivy.uix.treeview import TreeViewLabel
 #from kivy.uix.scrollview import ScrollView
-from kivy.clock import Clock
-#import time
 
-from kivy.uix.progressbar import ProgressBar
-#from kivy.clock import Clock
-
-#from kivy.uix.bubble import Bubble
-#from kivy.uix.bubble import BubbleButton
-
-from kivy.storage.jsonstore import JsonStore
-
+#Declaration of variables:
+selected = list()		
+topic = str('Visions')		
+the_screenmanager = ScreenManager()
+endtime = 0  
 idata = JsonStore('itemdata.json')
 idatacpy = dict(JsonStore('itemdata.json'))
 newdict = dict()
-#{u'abc': {u'itemtype': u'Visions', u'name': u'abc'}, u'def': {u'itemtype': u'Visions', u'name': u'def'}}
 for key in idatacpy:
 	counting = 0
 	subdict = idatacpy[key]
@@ -51,30 +45,6 @@ for key in idatacpy:
 			thekey=str()
 			thevalue=str()
 			counting = 0
-#print newdict
-#into dict x:y
-
-
-#print newdict
-#idata.put('item', Visions='')
-#idata.put('Missions', item='')
-#idata.put('Objectives', item='')
-		#inpt=TextInput(text=settingdata.get('email')['address'], multiline=False)
-		#store_btn.bind(on_release=(lambda store_btn: self.change_mail(inpt.text, popup1)))
-	#def change_mail(self, theaddress, popup1):
-		#the_screenmanager.current = 'planscreen'
-		#settingdata.put('email', address=theaddress)
-
-selected = list()		
-		
-#class TreeViewButton(Button, TreeViewNode):
-#	pass
-
-#thedate = thedate=datetime.now().strftime("%Y%m%d")
-#markedlines=list()
-
-#my_screenmanager = ScreenManager()
-mngr="start"
 
 Builder.load_string('''
 <StartScreen>:
@@ -162,52 +132,47 @@ Builder.load_string('''
     Button:
         size_hint: 1, 0.10
         pos_hint: {'x': 0, 'y': 0.90}
+        on_release: root.chngIscreen()
+        #on_release: app.root.current = 'itemscreen'
         text: "Item"
-        on_release: app.root.current = 'visitems'
     PlanExe:
     Button:
         size_hint: 1, 0.10
         pos_hint: {'x': 0, 'y': 0}
         text: "Exit"
         on_release: app.root.current = 'startscreen'
-<PlanExe>:
-    size_hint: 1, 0.8
-    pos_hint: {'x': 0, 'y': 0.10}
-    anchor_x: 'center'
-    anchor_y: 'top'
-    orientation: 'vertical'
-    padding: 50
-<VisItems>:
-    name:'visitems'
+<ItemScreen>:
+    name: 'itemscreen'
     BoxLayout:
-        #visspinner:visspinner
         anchor_x: 'center'
         anchor_y: 'top'
         orientation: 'vertical'
         padding: 50
-        Spinner:
-            id:visspinner
-            # default value shown
-            text:'Visions'
-            # available values
-            values:('Visions', 'Missions', 'Objectives')
-            # just for positioning in our example
-            size_hint:None, None
-            size:100, 44
-            #pos_hint:'center_x': .5, 'center_y': .5
-            
-            on_release: root.select_screen()
+		BoxLayout:
+		    orientation:'horizontal'
+            Button:
+                id: vis_button
+                text:'Visions'
+                on_release: root.vises()
+            Button:
+                id: mis_button
+                text:'Missions'
+                on_release: root.mises()
+            Button:
+                id: obj_button
+                text:'Objectives'
+                on_release: root.objes()
         BoxLayout:
-		    id:vismsspinner
+		    id:msspinner
             size_hint:(None, None)
             size:(100, 44)
             pos_hint:{'center_x': .5, 'center_y': .5}		    
         Button:
             text:'Select and deselect to statement'
-            on_release:root.slct_item(visspinner.text, root.visslctid)
+            on_release:root.slct_item(visspinner.text, root.slctid)
         Button:
             text:'Remove'
-            on_release:root.rmv_vis(visspinner.text, root.visslctid)
+            on_release:root.rmv_vis(visspinner.text, root.slctid)
         BoxLayout:
             orientation: 'horizontal'
             TextInput:
@@ -220,97 +185,18 @@ Builder.load_string('''
                 on_release:root.add_vis(visspinner.text, visinpt.text)                
         Button:
             text:'Cancel'
-            on_release:app.root.current = 'planscreen'
-<MisItems>:
-    name:'misitems'
-    BoxLayout:
-        #misspinner:misspinner
-        anchor_x: 'center'
-        anchor_y: 'top'
-        orientation: 'vertical'
-        padding: 50
-        Spinner:
-            id:misspinner
-            # default value shown
-            text:'Missions'
-            # available values
-            values:('Visions', 'Missions', 'Objectives')
-            # just for positioning in our example
-            size_hint:None, None
-            size:100, 44
-            #pos_hint:'center_x': .5, 'center_y': .5
-            on_release: root.select_screen()
-            
-        MultiSelectSpinner:
-            id:misslctid
-            size_hint:(None, None)
-            size:(100, 44)
-            #pos_hint:{'center_x': .5, 'center_y': .5}
-            values : root.newitems            
-        Button:
-            text:'Select and deselect to statement'
-            on_release:root.slct_item(misspinner.text, misslctid)
-        Button:
-            text:'Remove'
-            on_release:root.rmv_mis(misspinner.text, misslctid)
-        Button:
-            text:'Add item'
-            on_release:root.add_mis(misspinner.text, misinpt.text)
-        TextInput:
-            text:''
-            id:misinpt
-        Button:
-            text:'Cancel'
-            on_release:app.root.current = 'planscreen'
-<ObjItems>:
-    name:'objitems'
-    BoxLayout:
-        #objspinner:objspinner
-        anchor_x: 'center'
-        anchor_y: 'top'
-        orientation: 'vertical'
-        padding: 50
-        Spinner:
-            id:objspinner
-            # default value shown
-            text:'Objectives'
-            # available values
-            values:('Visions', 'Missions', 'Objectives')
-            # just for positioning in our example
-            size_hint:None, None
-            size:100, 44
-            #pos_hint:'center_x': .5, 'center_y': .5
-            
-            on_release: root.select_screen()
-            
-        MultiSelectSpinner:
-            id:objslctid
-            size_hint:None, None
-            size:100, 44
-            pos_hint:{'center_x': .5, 'center_y': .5}
-            values : root.newitems            
-        Button:
-            text:'Select and deselect to statement'
-            on_release:root.slct_item(objspinner.text, objslctid)
-        Button:
-            text:'Remove'
-            on_release:root.rmv_obj(objspinner.text, objslctid)
-        Button:
-            text:'Add item'
-            on_release:root.add_obj(objspinner.text, objinpt.text)
-        TextInput:
-            text:''
-            id:objinpt
-        Button:
-            text:'Cancel'
-            on_release:app.root.current = 'planscreen'
-    
+            on_release:app.root.current = 'planscreen'    
 <MultiSelectOption@ToggleButton>:
     size_hint: 1, None
     height: '48dp'
-''')
-the_screenmanager = ScreenManager()
-endtime = 0    
+<PlanExe>:
+    size_hint: 1, 0.8
+    pos_hint: {'x': 0, 'y': 0.10}
+    anchor_x: 'center'
+    anchor_y: 'top'
+    orientation: 'vertical'
+    padding: 50
+''')  
 
 class MultiSelectSpinner(Button):
 	"""Widget allowing to select multiple text options."""
@@ -364,44 +250,89 @@ class MultiSelectSpinner(Button):
 
 class PlanScreen(Screen):
 	global selected
-	#global idata
 	global the_screenmanager
-	global mngr	
+	
 	def __init__ (self,**kwargs):
 		super (PlanScreen, self).__init__(**kwargs)
+	def chngIscreen(self):
+		global the_screenmanager
+		the_screenmanager.current = 'itemscreen'
 		
-class VisItems(Screen):
+class ItemScreen(Screen):
 	global selected
-	#global idata
 	global the_screenmanager
-	global mngr
 	global newdict
+	global topic
 	
-	newitems=ListProperty()
-
-	visslctid = MultiSelectSpinner(
-		id="visslctid",
+	#problemline:
+	
+	newitems = ListProperty()
+	
+	slctid = MultiSelectSpinner(
+		id="slctid",
 		#now
 		#values = newitems
 	)
 
 	def __init__(self, **kwargs):
-		global selected
-		#global idata
+		
+		super (ItemScreen, self).__init__(**kwargs)
+		self.vises()
+		
+	def vises(self):
+		#now
+		global topic
 		global the_screenmanager
-		global mngr
+		global newdict
+		#super (Items, self).__init__(**kwargs)
+		for thename in newdict:
+			if str(newdict[thename])==str(topic):
+			#if str(newdict[name])==str(self.ids.visspinner.text):
+				self.newitems.append(thename)
+		#spinner for selecting items:
+		self.slctid.values = self.newitems
+		
+		#try:
+		#	self.ids.msspinner.add_widget(self.slctid)
+		#except:
+		#	pass
+
+	def mises(self):
+		global topic
+		global the_screenmanager
 		global newdict
 		newitems=str()
-		super (VisItems, self).__init__(**kwargs)
-		
-		for name in newdict:
-			if str(newdict[name])==str(self.ids.visspinner.text):
-				self.newitems.append(name)
-		self.visslctid.values = self.newitems
-		try:
-			self.ids.vismsspinner.add_widget(self.visslctid)
-		except:
-			pass
+		#super (VisItems, self).__init__(**kwargs)
+		for thename in newdict:
+			if str(newdict[thename])==str(topic):
+			#if str(newdict[thename])==str(self.ids.visspinner.text):
+				self.newitems.append(thename)
+		#spinner for selecting items:
+		self.slctid.values = self.newitems
+		#try:
+		#	self.ids.msspinner.add_widget(self.slctid)
+		#except:
+		#	pass
+
+
+	def objes(self):
+		global topic
+		global the_screenmanager
+		global newdict
+		newitems=str()
+		#super (VisItems, self).__init__(**kwargs)
+		for thename in newdict:
+			if str(newdict[thename])==str(topic):
+			#if str(newdict[thename])==str(self.ids.visspinner.text):
+				self.newitems.append(thename)
+		#spinner for selecting items:
+		self.slctid.values = self.newitems
+		#try:
+		#	self.ids.msspinner.add_widget(self.slctid)
+		#except:
+		#	pass
+
+
 	def slct_item(self,varitemtype,slctid):
 		global selected
 		del selected[:]
@@ -427,31 +358,31 @@ class VisItems(Screen):
 					my_dict.pop(str(i), None)
 					#self.newitems.remove(str(i))
 
-		self.ids.vismsspinner.remove_widget(self.visslctid)		
-		self.visslctid.values = self.newitems
-		self.ids.vismsspinner.add_widget(self.visslctid)		
+		self.ids.msspinner.remove_widget(self.slctid)		
+		self.slctid.values = self.newitems
+		self.ids.msspinner.add_widget(self.slctid)		
 		App.get_running_app().stop()
 		GnomieApp().run()
-		the_screenmanager.current = 'visitems'
+		the_screenmanager.current = 'itemscreen'
 
 	def add_vis(self, varitemtype, theitem):
 		global newdict
+		global the_screenmanager
 		idata.put(str(theitem), itemtype=varitemtype, name=theitem)
 		newdict[theitem] = varitemtype
 
-		self.ids.vismsspinner.remove_widget(self.visslctid)		
-		self.visslctid.values = self.newitems
-		self.ids.vismsspinner.add_widget(self.visslctid)		
+		self.ids.msspinner.remove_widget(self.slctid)		
+		self.slctid.values = self.newitems
+		self.ids.msspinner.add_widget(self.slctid)		
 		App.get_running_app().stop()
 		GnomieApp().run()
-		the_screenmanager.current = 'visitems'
+		the_screenmanager.current = 'itemscreen'
 
 	def Exit(self):
 		global the_screenmanager
-		global mngr
+		
 		self.nowpart = 0
 		self.nowtime=0						
-		mngr = 'start'
 		the_screenmanager.current = 'planscreen'
 
 	#def select_screen(self, spinner, thetext):
@@ -463,164 +394,32 @@ class VisItems(Screen):
 		global the_screenmanager
 		if name=='Visions':
 			#the_screenmanager.current = 'planscreen'
-			the_screenmanager.current='visitems'
-		if name=='Missions':
+			the_screenmanager.current='itemscreen'
+		#if name=='Missions':
 			#the_screenmanager.current = 'planscreen'
-			the_screenmanager.current='misitems'
-		if name=='Objectives':
+			#the_screenmanager.current='misitems'
+		#if name=='Objectives':
 			#the_screenmanager.current = 'planscreen'
-			the_screenmanager.current='objitems'			
-			
-class MisItems(Screen):
-	global selected
-	#global idata
-	global the_screenmanager
-	global mngr
-
-	newitems=ListProperty()
-	def __init__(self, **kwargs):
-		global selected
-		#global idata
-		global the_screenmanager
-
-		global mngr
-
-		super (MisItems, self).__init__(**kwargs)
-		#idata = JsonStore('itemdata.json')
-		#for anitem, atype in newdict.find(itemtype=str(self.ids.misspinner.text)):
-		#	self.newitems.append(anitem)
-		#now
-		#https://stackoverflow.com/questions/27809703/kivy-sending-text-from-spinner-to-another-function#27810290				
-
-	def slct_item(self,varitemtype,slctid):
-		global selected
-		del selected[:]
-		selected.append(varitemtype)
-		selected.extend(slctid.text.split(", "))
-		#the_screenmanager.current = 'misitems'
-		the_screenmanager.current = 'planscreen'
-		
-	def add_mis(self,varitemtype,slctid):
-		global selected
-		del selected[:]
-		selected=slctid.text.split(", ")
-		for i in selected:
-			idata.delete(str(i))
-		the_screenmanager.current = 'misitems'
-
-	def add_mis(self, varitemtype, theitem):
-		idata.put(str(theitem), itemtype=varitemtype, name=theitem)
-		the_screenmanager.current = 'misitems'
-
-	def Exit(self):
-		global the_screenmanager
-		global mngr
-		self.nowpart = 0
-		self.nowtime=0						
-		mngr = 'start'
-		the_screenmanager.current = 'planscreen'
-
-	#def select_screen(self, spinner, thetext):
-	def select_screen(self):
-		self.show(self.ids.misspinner.text)
-
-	#def show(self, name, popup1):
-	def show(self, name):
-		global the_screenmanager
-		if name=='Visions':
-			#the_screenmanager.current = 'planscreen'
-			the_screenmanager.current = 'visitems'
-		if name=='Missions':
-			#the_screenmanager.current = 'planscreen'
-			the_screenmanager.current = 'misitems'
-		if name=='Objectives':
-			#the_screenmanager.current = 'planscreen'
-			the_screenmanager.current = 'objitems'			
-			
-class ObjItems(Screen):
-	global selected
-	#global idata
-	global the_screenmanager
-	global mngr
-	newitems=ListProperty()
-
-	def __init__(self, **kwargs):
-		global selected
-		#global idata
-		global the_screenmanager
-		global mngr
-
-		super (ObjItems, self).__init__(**kwargs)
-		#idata = JsonStore('itemdata.json')
-		#for anitem, atype in newdict.find(itemtype=str(self.ids.objspinner.text)):
-		#	self.newitems.append(anitem)
-
-	def slct_item(self,varitemtype,slctid):
-		global selected
-		del selected[:]
-		selected.append(varitemtype)
-		selected.extend(slctid.text.split(", "))
-		#the_screenmanager.current = 'objitems'
-		the_screenmanager.current = 'planscreen'
-		
-	def rmv_obj(self,varitemtype,slctid):
-		global selected
-		del selected[:]
-		selected=slctid.text.split(", ")
-		for i in selected:
-			idata.delete(str(i))
-		the_screenmanager.current = 'objitems'
-
-	def add_obj(self, varitemtype, theitem):
-		the_screenmanager.current = 'planscreen'
-		idata.put(str(theitem), itemtype=varitemtype, name=theitem)
-		the_screenmanager.current = 'objitems'
-
-	def Exit(self):
-		global the_screenmanager
-		global mngr
-		self.nowpart = 0
-		self.nowtime=0						
-		mngr = 'start'
-		the_screenmanager.current = 'planscreen'
-
-	#def select_screen(self, spinner, thetext):
-	def select_screen(self):
-		self.show(self.ids.objspinner.text)
-
-	#def show(self, name, popup1):
-	def show(self, name):
-		global the_screenmanager
-		if name=='Visions':
-			#the_screenmanager.current = 'planscreen'
-			the_screenmanager.current='visitems'
-		if name=='Missions':
-			#the_screenmanager.current = 'planscreen'
-			the_screenmanager.current='misitems'
-		if name=='Objectives':
-			#the_screenmanager.current = 'planscreen'
-			the_screenmanager.current='objitems'			
+			#the_screenmanager.current='objitems'			
 			
 class MFScreen(Screen):
 	global endtime
 	global the_screenmanager
-	global mngr
+	
 	def __init__ (self,**kwargs):
 		global endtime
 		super (MFScreen, self).__init__(**kwargs)
 	def two(self):
 		global the_screenmanager
 		global endtime
-		global mngr
-		mngr='exescreen'		
+		
 		endtime=120
 		the_screenmanager.current = 'exescreen'
 
 	def four(self):
 		global the_screenmanager
 		global endtime
-		global mngr
-		mngr='exescreen'		
+		
 		endtime=240
 		the_screenmanager.current = 'exescreen'
 
@@ -628,8 +427,7 @@ class ExeScreen(Screen):
 	pass		
 
 class PlanExe(TabbedPanel):
-#now:
-	global mngr
+	
 	global the_screenmanager
 	global endtime
 	global selected
@@ -658,7 +456,7 @@ class PlanExe(TabbedPanel):
 		Clock.schedule_interval(self.planupdate, 0.2)
 
 	def planupdate(self, dt):
-		global mngr
+		
 		global selected
 		#print selected
 		self.box.clear_widgets()
@@ -692,7 +490,7 @@ class PlanExe(TabbedPanel):
 		newdict['Statements'] = 'selectedstr'		
 			
 class MFExe(BoxLayout):
-	global mngr
+	
 	global the_screenmanager
 	global endtime
 	nowtime = NumericProperty(0)
@@ -710,16 +508,16 @@ class MFExe(BoxLayout):
 		
 	def update(self, dt):
 		self.limit=endtime
-		global mngr
-		if mngr == "exescreen":
-			if self.nowtime >= self.limit and self.nowpart < 5 and self.nowpart >= 0:
-				if self.nowpart == 5:
-					self.nowtime = 0
-				else:
-					self.nowpart+=1
-					self.nowtime = 0
-			elif self.nowpart != 5:		
-				self.nowtime+=1
+#		
+#		if mngr == "exescreen":
+#			if self.nowtime >= self.limit and self.nowpart < 5 and self.nowpart >= 0:
+#				if self.nowpart == 5:
+#					self.nowtime = 0
+#				else:
+#					self.nowpart+=1
+#					self.nowtime = 0
+#			elif self.nowpart != 5:		
+#				self.nowtime+=1
 		#print self.nowtime
 		#print mngr
 		#print self.limit
@@ -736,10 +534,9 @@ class MFExe(BoxLayout):
 			
 	def Exit(self):
 		global the_screenmanager
-		global mngr
+		
 		self.nowpart = 0
 		self.nowtime=0						
-		mngr = 'start'
 		the_screenmanager.current = 'mfscreen'
 			
 #https://stackoverflow.com/questions/40341674/update-kivy-label-using-screen-manager
@@ -768,30 +565,25 @@ class StartScreen(Screen):
 
 	def Plan(self):
 		pass
-		
+
 class GnomieApp(App):
-	global my_screenmanager
+	global the_screenmanager
 	def build(self):
-		global my_screenmanager
+		global the_screenmanager
 		#the_screenmanager = ScreenManager()
 		startscreen = StartScreen(name='startscreen')
 		reviewscreen = ReviewScreen(name='reviewscreen')
 		mfscreen = MFScreen(name='mfscreen')
 		exescreen = ExeScreen(name='exescreen')
 		planscreen = PlanScreen(name='planscreen')
-		visitems = VisItems(name='visitems')
-		misitems = MisItems(name='misitems')
-		objitems = ObjItems(name='objitems')
+		itemscreen = ItemScreen(name='itemscreen')
 		
-		#if mngr=='start':
 		the_screenmanager.add_widget(startscreen)
 		the_screenmanager.add_widget(reviewscreen)
 		the_screenmanager.add_widget(mfscreen)
 		the_screenmanager.add_widget(exescreen)
 		the_screenmanager.add_widget(planscreen)
-		the_screenmanager.add_widget(visitems)
-		the_screenmanager.add_widget(misitems)
-		the_screenmanager.add_widget(objitems)
+		the_screenmanager.add_widget(itemscreen)
 		return the_screenmanager
 		
 if __name__ == '__main__':
