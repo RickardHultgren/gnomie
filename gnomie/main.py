4b0cb16c55f6e61213e2868eadc27348d250e5ee
Rickard Hultgren <sababa.sababa@gmail.com>
2018-03-16 12:18:09 +0000

    Update main.py

--- gnomie/main.py
+++ gnomie/main.py
@@ -431,14 +431,18 @@ class MainScreen(Screen):
 			self.ids.main_box.add_widget(cat_lbl)
 			
 			rubric_box=BoxLayout(orientation="horizontal", size_hint_y=None, size_hint_x=1, size=(self.ids.main_box.width, "%ssp"%str(1*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
-			temp_dict = {"obj":"Objective","mis":"Mission","vis":"Vision"}
-			for temp_word in temp_dict:
-				eval("%s_btn"%temp_word)=Button(text=eval("%s_btn"%temp_dict), size_hint_y=None, size_hint_x=None, size=(0.33*self.ids.main_box.width, "%ssp"%str(2*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2),background_color= (.25, .75, 1.0, 1.0))
-				eval("%s_btn"%temp_word).bind(on_release=lambda eval("%s_btn"%temp_word): self.eval("chng_%s()"%temp_word))
+			obj_btn=Button(text="Objectives", size_hint_y=None, size_hint_x=None, size=(0.33*self.ids.main_box.width, "%ssp"%str(2*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2),background_color= (.25, .75, 1.0, 1.0))
+			obj_btn.bind(on_release=lambda obj_btn: self.chng_obj())
+			mis_btn=Button(text="Missions", size_hint_y=None, size_hint_x=None, size=(0.33*self.ids.main_box.width, "%ssp"%str(2*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2),background_color= (.25, .75, 1.0, 1.0))
+			mis_btn.bind(on_release=lambda mis_btn: self.chng_mis())
+			vis_btn=Button(text="Visions", size_hint_y=None, size_hint_x=None, size=(0.33*self.ids.main_box.width, "%ssp"%str(2*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2),background_color= (.25, .75, 1.0, 1.0))
+			vis_btn.bind(on_release=lambda vis_btn: self.chng_vis())
+
 			eval("%s_btn"%self.state_topic).background_color= (.75, .25, 0, 1.0)
-			for temp_word in temp_dict:
+			temp_list=["obj","vis","mis"]
+			for temp_word in temp_list:
 				rubric_box.add_widget(eval("%s_btn"%temp_word))
-			temp_dict = None
+			temp_list = None
 			self.ids.main_box.add_widget(rubric_box)
 
 			res_box=GridLayout(cols=2,size_hint_y=None, size_hint_x=1, size=(self.ids.main_box.width, "%ssp"%str(4*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
@@ -465,10 +469,18 @@ class MainScreen(Screen):
 			self.ids.main_box.add_widget(bttn_box)
 			
 			for preNomen in ["obj","mis","vis"]:
-				eval("%s_lbl"%preNomen)
-				eval("%s_lbl"%preNomen)=Label(text="",size_hint_y=None, size_hint_x=1, size=(self.ids.main_box.width, "%ssp"%str(1*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
-				self.ids.main_box.add_widget(eval("%s_lbl"%preNomen))
-				self.ids.main_box.height += eval("%s_lbl"%preNomen).height
+				if preNomen == "obj":
+					obj_lbl=Label(text="",size_hint_y=None, size_hint_x=1, size=(self.ids.main_box.width, "%ssp"%str(1*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
+					self.ids.main_box.add_widget(obj_lbl)
+					self.ids.main_box.height += obj_lbl.height
+				if preNomen == "mis":
+					mis_lbl=Label(text="",size_hint_y=None, size_hint_x=1, size=(self.ids.main_box.width, "%ssp"%str(1*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
+					self.ids.main_box.add_widget(mis_lbl)
+					self.ids.main_box.height += mis_lbl.height
+				if preNomen == "vis":
+					vis_lbl=Label(text="",size_hint_y=None, size_hint_x=1, size=(self.ids.main_box.width, "%ssp"%str(1*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))					
+					self.ids.main_box.add_widget(vis_lbl)
+					self.ids.main_box.height += vis_lbl.height
 				for pop_item in self.pop_choices[self.topic][1]:
 					if self.pop_choices[self.topic][1][pop_item]["state"] == self.state_claim:
 						if self.pop_choices[self.topic][1][pop_item]["nomen"] == preNomen and self.state_topic==preNomen:
@@ -494,44 +506,44 @@ class MainScreen(Screen):
 								res_edit.bind(on_release = partial(self.edit_post, pop_item))
 								res_box.add_widget(res_edit)
 								res_box.height += res_edit.height
-							#res_del.bind(on_release = partial(self.del_nomen, pop_item))
-							try:
-								begin_box = BoxLayout(orientation="horizontal",size_hint_y=None, size_hint_x=1, size=(self.ids.main_box.width, "%ssp"%str(1*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
-								beginning = self.pop_choices[self.topic][1][pop_item]["begin"]
-								begin_lbl=Label(text="begin: %s"%beginning, size_hint_y=None, size_hint_x=None, size=(0.75*self.ids.main_box.width, "%ssp"%str(1*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
-								if self.pop_choices[self.topic][1][pop_item]["begin"] != "add" or self.pop_choices[self.topic][1][pop_item]["begin"] != "" :
-									begin_box.add_widget(begin_lbl)
-									self.ids.main_box.add_widget(begin_box)
-							except:
-								pass
-							try:
-								end_box = BoxLayout(orientation="horizontal",size_hint_y=None, size_hint_x=1, size=(self.ids.main_box.width, "%ssp"%str(1*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
-								ending = self.pop_choices[self.topic][1][pop_item]["end"]
-								end_lbl=Label(text="end: %s"%ending, size_hint_y=None, size_hint_x=None, size=(0.75*self.ids.main_box.width, "%ssp"%str(1*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
-								if self.pop_choices[self.topic][1][pop_item]["end"] != "add" or self.pop_choices[self.topic][1][pop_item]["end"] != "" :
-									end_box.add_widget(end_lbl)
-									self.ids.main_box.add_widget(end_box)
-							except:
-								pass
-							try:
-								mood_box = BoxLayout(orientation="horizontal",size_hint_y=None, size_hint_x=1, size=(self.ids.main_box.width, "%ssp"%str(1*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
-								moodning = self.pop_choices[self.topic][1][pop_item]["mood"]
-								mood_lbl=Label(text="mood: %s"%moodning, size_hint_y=None, size_hint_x=None, size=(0.75*self.ids.main_box.width, "%ssp"%str(1*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
-								if self.pop_choices[self.topic][1][pop_item]["mood"] != "":
-									mood_box.add_widget(mood_lbl)
-									self.ids.main_box.add_widget(mood_box)
-							except:
-								pass
-							try:
-								about_box = BoxLayout(orientation="horizontal",size_hint_y=None, size_hint_x=1, size=(self.ids.main_box.width, "%ssp"%str(1*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
-								abouting = self.pop_choices[self.topic][1][pop_item]["about"]
-								about_lbl=Label(text="about: %s"%abouting, size_hint_y=None, size_hint_x=None, size=(0.75*self.ids.main_box.width, "%ssp"%str(1*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
-								if self.pop_choices[self.topic][1][pop_item]["about"] != "":
-									about_box.add_widget(about_lbl)
-									self.ids.main_box.add_widget(about_box)
-							except:
-								pass
-						
+								#res_del.bind(on_release = partial(self.del_nomen, pop_item))
+								try:
+									begin_box = BoxLayout(orientation="horizontal",size_hint_y=None, size_hint_x=1, size=(self.ids.main_box.width, "%ssp"%str(1*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
+									beginning = self.pop_choices[self.topic][1][pop_item]["begin"]
+									begin_lbl=Label(text="begin: %s"%beginning, size_hint_y=None, size_hint_x=None, size=(0.75*self.ids.main_box.width, "%ssp"%str(1*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
+									#if self.pop_choices[self.topic][1][pop_item]["begin"] != "add" or self.pop_choices[self.topic][1][pop_item]["begin"] != "" :
+									#	begin_box.add_widget(begin_lbl)
+									#	self.ids.main_box.add_widget(begin_box)
+								except:
+									pass
+								try:
+									end_box = BoxLayout(orientation="horizontal",size_hint_y=None, size_hint_x=1, size=(self.ids.main_box.width, "%ssp"%str(1*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
+									ending = self.pop_choices[self.topic][1][pop_item]["end"]
+									end_lbl=Label(text="end: %s"%ending, size_hint_y=None, size_hint_x=None, size=(0.75*self.ids.main_box.width, "%ssp"%str(1*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
+									#if self.pop_choices[self.topic][1][pop_item]["end"] != "add" or self.pop_choices[self.topic][1][pop_item]["end"] != "" :
+									#	end_box.add_widget(end_lbl)
+									#	self.ids.main_box.add_widget(end_box)
+								except:
+									pass
+								try:
+									mood_box = BoxLayout(orientation="horizontal",size_hint_y=None, size_hint_x=1, size=(self.ids.main_box.width, "%ssp"%str(1*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
+									moodning = self.pop_choices[self.topic][1][pop_item]["mood"]
+									mood_lbl=Label(text="mood: %s"%moodning, size_hint_y=None, size_hint_x=None, size=(0.75*self.ids.main_box.width, "%ssp"%str(1*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
+									if self.pop_choices[self.topic][1][pop_item]["mood"] != "":
+										mood_box.add_widget(mood_lbl)
+										self.ids.main_box.add_widget(mood_box)
+								except:
+									pass
+								try:
+									about_box = BoxLayout(orientation="horizontal",size_hint_y=None, size_hint_x=1, size=(self.ids.main_box.width, "%ssp"%str(1*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
+									abouting = self.pop_choices[self.topic][1][pop_item]["about"]
+									about_lbl=Label(text="about: %s"%abouting, size_hint_y=None, size_hint_x=None, size=(0.75*self.ids.main_box.width, "%ssp"%str(1*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
+									if self.pop_choices[self.topic][1][pop_item]["about"] != "":
+										about_box.add_widget(about_lbl)
+										self.ids.main_box.add_widget(about_box)
+								except:
+									pass
+							
 							self.ids.main_box.add_widget(res_box)
 							self.ids.main_box.height += res_box.height
 				#res_box=BoxLayout(size_hint_y=None, size_hint_x=1, size=(self.ids.main_box.width, "%ssp"%str(2*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
@@ -844,16 +856,14 @@ class MainScreen(Screen):
 		
 		#if self.state_topic=="obj" and args>2:
 		#if self.state_topic=="obj":
-		try:
-			begin2var=str(args[0].text)
-		except:
-			begin2var=""
-		try:
-			end2var=str(args[0].text)
-		except:
-			end2var=""
+		begin2var=""
+		end2var=""
 		mood2var=""
 		about2var=""
+		
+		if len(args)>2:
+			begin2var=str(args[0].text)
+			end2var=str(args[1].text)
 ###!		
 		think_things.put("%s"%maxed, title=res, state=self.state_claim, nomen=preNomen, begin=begin2var, end=end2var, mood=mood2var, about=about2var)
 		think_things_cpy["%s"%maxed]={"title":res, "state":self.state_claim, "nomen":preNomen, "begin":begin2var, "end":end2var}
@@ -1139,6 +1149,8 @@ class MainScreen(Screen):
 					else:
 						think_things.put("%s"%(key), title=title_var, state=state_var, nomen=new_res, begin=begin_var, end=end_var)
 						think_things_cpy["%s"%(key)] = {"title":title_var, "state":state_var, "nomen":new_res, "begin":begin_var, "end":end_var}
+					#think_things.put("%s"%(key), title=title_var, state=state_var, nomen=new_res, begin=begin_var, end=end_var)
+					#think_things_cpy["%s"%(key)] = {"title":title_var, "state":state_var, "nomen":new_res, "begin":begin_var, "end":end_var}
 
 
 		eval("%s_things"%self.topic).delete(res)
