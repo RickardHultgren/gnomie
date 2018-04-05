d3d90d08f4236a00c8033fd2bc31455a19f66dda
Rickard Hultgren <sababa.sababa@gmail.com>
2018-04-05 16:11:03 +0000

    Update main.py

--- gnomie/main.py
+++ gnomie/main.py
@@ -53,7 +53,26 @@ if platform == 'android':
 	
 	intent = Intent()
 	#calendar = Calendar.getInstance()
-	
+
+	#Intent intent = new Intent(Intent.ACTION_INSERT)
+	intent.setType("vnd.android.cursor.item/event")
+	#intent.putExtra(Events.TITLE, "Learn Android")
+	##intent.putExtra(Events.EVENT_LOCATION, "Home suit home")
+	##intent.putExtra(Events.DESCRIPTION, "Download Examples")
+	# Setting dates GregorianCalendar calDate = new GregorianCalendar(2012, 10, 02)
+	##intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, calDate.getTimeInMillis())
+	##intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME, calDate.getTimeInMillis())
+	# Make it a full day event intent.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, true)
+	# Make it a recurring Event intent.putExtra(Events.RRULE, "FREQ=WEEKLY
+	#COUNT=11
+	#WKST=SU
+	#BYDAY=TU,TH")
+	# Making it private and shown as busy intent.putExtra(Events.ACCESS_LEVEL, Events.ACCESS_PRIVATE)
+	##intent.putExtra(Events.AVAILABILITY, Events.AVAILABILITY_BUSY)
+	#startActivity(intent)
+
+	 
+		
 	#calendar.setTimeInMillis(1480103863835)
 	#Logger.info(calendar.getTimeInMillis()) //  -1659853285
 	#intent.setType("vnd.android.cursor.item/event");
