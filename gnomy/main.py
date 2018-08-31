195b1eedbffd3d7d644c0c4d70a96399349a1f4d
Rickard Hultgren <sababa.sababa@gmail.com>
2018-08-31 03:11:33 +0000

    Update main.py

--- gnomy/main.py
+++ gnomy/main.py
@@ -217,7 +217,7 @@ Builder.load_string('''
                     #title: ''
                     app_icon: 'gnomy.png'
                     with_previous: False
-                    on_release: root.start()
+                    on_release: root.mindf()
                     #on_press: root.start()
                     #background_normal: ''
                     #background_color: 1, .3, .4, .85
@@ -349,6 +349,39 @@ class MainScreen(Screen):
 	parts = ["Relax and breathe calmly","breathe in and out","Feel the muscles of the head and neck","of the the arms","of the chest and abdomen","of the legs","Feel the organs of the pelvis and abdomen","Feel the organs of the chest","Feel the sensations of light","smell","taste","sound","touch","Feel inner state and mood","Try to name the mood you feel","Try to feel all the feelings all at once","End of mindfulness"]
 	#parts = ["Breath calm and Relax muscles","Feel muscles and organs","Feel sensations","Feel inner state","Feel inner awareness","End of mindfulness"]
 
+	def prevb(self, *args):
+		if self.mindf_part != 0:
+			self.mindf_part -= 1
+		self.mindf_time = 0
+
+	def nxtb(self, *args):
+		if self.mindf_part != len(self.parts)-1:
+			self.mindf_part += 1
+		else:
+			self.mindf_part = 0
+		self.mindf_time = 0
+
+	def pausing(self, *args):
+		if self.paused == False:
+			self.paused = True
+			Clock.unschedule(self.planupdate)
+			self.planupdate()
+		else:
+			self.paused = False
+			Clock.schedule_interval(self.planupdate, 0.2)
+			
+	def exitb(self, *args):
+		self.mindf_part = 0
+		self.mindf_time = 0
+		self.topic="start"
+		self.planupdate()
+			
+	def start(self):
+		self.topic="start"
+		self.planupdate()
+	
+
+
 	def about(self):
 		try:
 			self.popbox.clear_widgets()
@@ -377,7 +410,42 @@ class MainScreen(Screen):
 		self.popbox.height += exit_bttn.height
 
 		self.popup1.open()
+
+	def settings(self):
+		self.topic="settings"
+		self.planupdate()
 	
+	def act_tts(self, checkbox, value):
+		if value:
+			#self.act_ttsvar = True
+			
+			#set_things_cpy.pop("tts", None)
+			#set_things.delete("tts")
+			temp_set_cpy['tts'] = "True"
+			setting_things.put("tts", tts="True")
+		else:
+			#self.act_ttsvar = False
+			temp_set_cpy['tts'] = "False"
+			setting_things.put("tts", tts="False")
+
+	def act_vibr(self, checkbox, value):
+		if value:
+			#self.act_vibrvar = True
+			
+			#set_things_cpy.pop("vibr", None)
+			#set_things.delete("vibr")
+			temp_set_cpy['vibr'] = "True"
+			setting_things.put("vibr", vibr="True")
+		else:
+			#self.act_vibrvar = False
+			temp_set_cpy['vibr'] = "False"
+			setting_things.put("vibr", vibr="False")
+			
+	def chng_setname(self, name_inpt, *args):
+		temp_set_cpy['setname'] = name_inpt.text
+		setting_things.put("setname", setname=name_inpt.text)
+
+
 	def __init__ (self,**kwargs):
 		super (MainScreen, self).__init__(**kwargs)
 		self.planupdate()
@@ -493,12 +561,12 @@ class MainScreen(Screen):
 
 		if self.topic == "mindf":
 			if self.going == True:
-				try:
-					vibrator.vibrate(0.125)
-					#vibrator.cancel()
-					
-				except:
-					pass
+				if temp_set_cpy['vibr'] == "True":
+					try:
+						vibrator.vibrate(0.125)
+						#vibrator.cancel()
+					except:
+						pass
 				if temp_set_cpy['tts'] == "True":
 					#print "tts: " + temp_set_cpy['tts']
 				#if self.act_ttsvar == True:	
@@ -528,6 +596,9 @@ class MainScreen(Screen):
 					self.going=True
 			elif self.mindf_part != len(self.parts)-1:		
 				self.mindf_time += self.mindf_speed
+				if not (self.mindf_time/self.mindf_speed)%60 :
+					self.going=True
+
 			
 		if self.topic == "state":
 
@@ -677,18 +748,331 @@ class MainScreen(Screen):
 		#main_box.height += 1*self.txt_height + Window.keyboard_height
 		#main_box.height += 1*self.txt_height + Window.height
 
+	def popping(self):
+		
+		poplbl=Label(halign="center", valign="top", text = self.pop_rubric, size_hint_y=None, size_hint_x=1, size=(self.ids.megabox.width, "%ssp"%str(self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
+		poplbl.bind(width=lambda s, w:
+		       s.setter('text_size')(s, (.66*self.width, None)))
+		poplbl.bind(height=poplbl.setter('texture_size[1]'))
+		poplbl.bind(height=poplbl.setter('self.minimum_height'))
+							
+		#self.popbox.add_widget(poplbl)
+		self.poptop.add_widget(poplbl)
+		#self.popbox.height += poplbl.height
+		self.poptop.height += poplbl.height
+		
+		### size_hint_x=1 prevents boxes from squeezing together
+		box1 = BoxLayout(orientation='horizontal', size_hint_y=None, width=.8*self.popbox.width, size_hint_x=1, size=(self.popbox.width, "%ssp"%str(2*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
+		box2 = BoxLayout(orientation='horizontal', size_hint_y=None, width=.8*self.popbox.width, size_hint_x=1, size=(self.popbox.width, "%ssp"%str(1*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
+		new_box=BoxLayout(size_hint_y=None, size_hint_x=1, size=(self.ids.megabox.width, "%ssp"%str(2*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
+		new_box_title = TextInput(text=self.pop_title_name, multiline=False, size_hint_x=1, size=(.5*box1.width, "%ssp"%str(2*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
+		new_box_unit = TextInput(text=self.pop_unit_name, multiline=False, size_hint_x=1, size=(.5*box1.width, "%ssp"%str(2*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
+		
+		if self.topic=="mindf":
+			new_box_unit.input_type='number'
+		box1.add_widget(Label(text = self.pop_title, size_hint_y=None, size_hint_x=1, size=(.5*box1.width, "%ssp"%str(2*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2)))
+		box1.add_widget(Label(text = self.pop_unit, size_hint_y=None, size_hint_x=1, size=(.5*box1.width, "%ssp"%str(2*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2)))
+		
+		box1.add_widget(Label(text = "", size_hint_y=None, size_hint_x=None, size=("%ssp"%str(10*self.txt_height), "%ssp"%str(2*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2)))
+		new_box_add=Button(text = self.pop_action, size_hint_y=None, size_hint_x=1, size=("%ssp"%str(10*self.txt_height), "%ssp"%str(2*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
+
+		new_box_add.bind(on_release=lambda new_box_add: self.add_new(new_box_title.text, new_box_unit.text))
+		
+		new_box.add_widget(new_box_title)
+#		with new_box_title.canvas.before:
+#			BorderImage(
+#				size=(new_box_title.width + 100, new_box_title.height + 100),
+#				pos=(new_box_title.x - 50, new_box_title.y - 50),
+#				border=(10, 10, 10, 10),
+#				source='left.png')
+
+		new_box.add_widget(new_box_unit)
+		new_box.add_widget(new_box_add)
+		#self.popbox.add_widget(box1)
+		#self.popbox.add_widget(new_box)
+		#self.popbox.add_widget(box2)
+		#self.popbox.height += box1.height
+		#self.popbox.height += box2.height
+			
+		self.poptop.add_widget(box1)
+		self.poptop.add_widget(new_box)
+		self.poptop.add_widget(box2)
+		self.poptop.height += box1.height
+		self.poptop.height += box2.height
+	
+		for pop_item in self.pop_choices[self.topic][0]: #Go through stored statements
+			popping_box=BoxLayout(size=(1*self.popbox.width, "%ssp"%str(2*self.txt_height)), orientation="horizontal", size_hint_y=None, size_hint_x=1, font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
+
+			if self.fontheight*(len(str(pop_item))/self.line_len) > 3*self.fontheight :
+				bttnheight=2*self.fontheight+self.fontheight*(len(str(pop_item))/self.line_len)
+			else:
+				bttnheight=3*self.fontheight
+			popping_box_title = Button(size=("%ssp"%str(10*self.txt_height), "%ssp"%str(2*self.txt_height)), height="%ssp"%str(bttnheight), text=str(pop_item), size_hint_y=None, size_hint_x=None, font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
+			popping_box_title.bind(width=lambda s, w:
+				s.setter('text_size')(s, (33*self.width, None)))
+			popping_box_title.bind(height=popping_box_title.setter('texture_size[1]'))
+			popping_box_title.bind(height=popping_box_title.setter('self.minimum_height'))
+
+			predict = self.pop_funcs[self.topic].__name__
+			popping_box_title.bind(on_release = partial(eval("self.%s"%(predict)),pop_item))
+		
+			if self.fontheight*(len(str("%s %s"%(str(self.pop_choices[self.topic][0][pop_item]),self.pop_unit_name)))/self.line_len) > 3*self.fontheight :
+				bttnheight=2*self.fontheight+self.fontheight*(len(str("%s %s"%(str(self.pop_choices[self.topic][0][pop_item]),self.pop_unit_name)))/self.line_len)
+			else:
+				bttnheight=3*self.fontheight
+			popping_box_min = Button(size=("%ssp"%str(10*self.txt_height), "%ssp"%str(2*self.txt_height)), text="%s %s"%(str(self.pop_choices[self.topic][0][pop_item]),self.pop_unit_name), size_hint_y=None, size_hint_x=None, font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
+			popping_box_min.bind(width=lambda s, w:
+				s.setter('text_size')(s, (self.width, None)))
+			popping_box_min.bind(height=popping_box_min.setter('texture_size[1]'))
+			popping_box_min.bind(height=popping_box_min.setter('self.minimum_height'))
+			popping_box_min.bind(on_release = partial(eval("self.%s"%(predict)),pop_item))
+
+			popping_box_del=Button(size_hint_y=1, size_hint_x=1, size=("%ssp"%str(5*self.txt_height), "%ssp"%str(2*self.txt_height)), text = '...', font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
+			
+			popping_box_del.bind(on_release = partial(self.menu, pop_item))
+			if popping_box_min.height>popping_box_title.height:
+				#popping_box.height=self.txt_height * 0.2
+				popping_box.height=2*popping_box_min.height
+				popping_box_del.height=2*popping_box_min.height
+			else:
+				popping_box.height=self.txt_height * 0.2
+				popping_box.height=2*popping_box_title.height
+				popping_box_del.height=2*popping_box_title.height
+			#popping_box.minimum_height="2ssp"
+			#popping_box.width=self.ids.megabox.width
+			popping_box.size=(self.ids.megabox.width, "%ssp"%str(2*self.txt_height))
+			#popping_box.size = Window.size
+			
+			popping_box.add_widget(popping_box_title)
+			popping_box.add_widget(popping_box_min)
+			
+			popping_box.add_widget(popping_box_del)
+			self.popbox.add_widget(popping_box)
+			self.popbox.height += popping_box.height
+		self.popup1.open()
+
+
+	def menu(self, pop_item, *args):
+		
+		
+		try:
+			self.popbox.clear_widgets()
+			self.poptop.clear_widgets()
+		except:
+			pass
+		try:
+			self.main_x_box.clear_widgets()
+		except:
+			pass			
+		try:
+			self.popup2.dismiss()
+		except:
+			pass
+		try:
+			self.popup1.dismiss()
+		except:
+			pass			
+
+
+		self.popup2.title=pop_item
+
+		self.pop_bubble.background_color =(20, 0, 0, .5) 
+		self.pop_bubble.border = [50, 50, 50, 10]
+		#self.pop_bubble.size = (150, 50)
+		self.pop_bubble.arrow_pos= 'top_mid'
+		my_bub_lbl=Label(text="Edit %s"%pop_item)
+		my_bub_title=TextInput(text="%s"%pop_item, multiline=False, size_hint_x=1, size_hint_y=None, size=(self.ids.megabox.width, "%ssp"%str(4*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
+		my_bub_cat=TextInput(text="%s"%str(self.pop_choices[self.topic][0][pop_item]), multiline=False, size_hint_x=1, size_hint_y=None, size=(self.ids.megabox.width, "%ssp"%str(4*self.txt_height)),font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
+		my_bub_bttn=BubbleButton(text="change", size_hint_x=1, size_hint_y=None, size=(self.ids.megabox.width, "%ssp"%str(4*self.txt_height)))
+		my_bub_bttn.bind(on_release=partial(self.chng_edit, pop_item, my_bub_title, my_bub_cat))
+		my_bub_btnN=BubbleButton(text="cancel", size_hint_x=1, size_hint_y=None, size=(self.ids.megabox.width, "%ssp"%str(4*self.txt_height)))
+		my_bub_btnN.bind(on_release=partial(eval("self.%s"%(self.topic) )))
+			
+		self.main_x_box.add_widget(my_bub_lbl)
+		self.main_x_box.add_widget(my_bub_title)
+		self.main_x_box.add_widget(my_bub_cat)
+		self.main_x_box.add_widget(my_bub_bttn)
+		self.main_x_box.add_widget(my_bub_btnN)
+
+
+		popping_box_del=Button(halign="left", valign="middle", text="del", size_hint_y=None, size_hint_x=None, font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
+		popping_box_del.width=.5*self.width
+		popping_box_del.bind(on_release = partial(self.del_pop, pop_item))
+		popping_box_slct=Button(text = 'select',size_hint_y=None, size_hint_x=None, size=("%ssp"%str(5*self.txt_height), "%ssp"%str(2*self.txt_height)), font_name="DejaVuSerif",spacing=(self.txt_height * 0.2, self.txt_height * 0.2))
+		predict = self.pop_funcs[self.topic].__name__
+		popping_box_slct.bind(on_release = partial(eval("self.%s"%(predict)),pop_item))
+
+		self.main_x_box.add_widget(popping_box_slct)
+		self.main_x_box.add_widget(popping_box_del)
+		#self.main_x_box.height += popping_box.height
+		self.popup2.open()
+		
+	def mindf(self, *args):
+		try:
+			self.popbox.clear_widgets()
+			self.poptop.clear_widgets()
+		except:
+			pass
+		self.topic="mindf"
+		self.popup1.title="mindfulness"
+		#if self.fontheight*(len(self.main_headline[self.topic])/self.line_len) > self.fontheight :
+		#	self.txt_height=0*self.fontheight+self.fontheight*(len(self.main_headline[topic])/self.line_len)
+		#else:
+		#	self.txt_height=self.fontheight		
+		self.pop_rubric = 'For how long time do you want to exercise mindfulness?'
+		self.pop_unit= "min"
+		self.pop_action = "add"
+		self.pop_title = "title"
+		self.pop_title_name = ""
+		self.pop_unit_name = ""
+		self.popping()
+
+
+
+	def start_timer(self, pop_item, *args):
+		timer_item_value=self.pop_choices[self.topic][0][pop_item]
+		#Timer_item is the number of minutes the mindfulness should take.
+		self.mindf_limit=200
+		try:
+			self.popup1.dismiss()
+		except:
+			pass
+		self.mindf_part = 0
+		self.mindf_time = 0
+		#print int(timer_item_value)
+		#300 * int(timer_item_value) [0.2 sec] / len(self.parts)[items] = 200 [points] / 1 [items]
+		#(300 * int(timer_item_value)) / (len(self.parts)) = 200[points]/[0.2 sec]
+		#(300 * int(timer_item_value))[0.2 sec] / (200*len(self.parts))[points] = 1
+		#(200*len(self.parts))[points] / (300 * int(timer_item_value))[0.2 sec] = 1
+		self.mindf_speed = (self.mindf_limit * len(self.parts)) / (float(timer_item_value) * 300)
+		Clock.unschedule(self.planupdate)		
+		Clock.schedule_interval(self.planupdate, 0.2)
+
+	def add_new(self,mindf_title,mindf_time):
+		checking=1
+		for pop_item in self.pop_choices[self.topic][0]:
+			if self.pop_choices[self.topic][0][pop_item] == mindf_title:
+				checking = 0
+		if checking == 1:
+			eval("%s_things"%self.topic).put(str(mindf_title), category=mindf_time, title=mindf_title)
+			self.pop_choices[self.topic][0][mindf_title] = mindf_time
+		eval("self.%s()"%self.topic)
+
+		
+	def chng_edit(self, res, new_res, cat, *args):
+		cat=cat.text
+		new_res=new_res.text
+		times_matched = 0
+		length = int(len(think_things_cpy)+1)
+		nowlen = 1
+		keylist=list(think_things_cpy.keys())
+
+
+		for key in sorted(keylist):
+
+				title_var = ""
+				state_var = ""
+				nomen_var = ""
+				for j in ["title","state","nomen"] :
+					if j == "title" :
+						title_var = str(think_things_cpy[key][j])
+					if j == "state" :
+						state_var = str(think_things_cpy[key][j])
+					if j == "nomen" :
+						nomen_var = str(think_things_cpy[key][j])
+				think_things.delete(key)
+				think_things_cpy.pop(key, None)
+				think_things.put("%s"%(nowlen), title=title_var, state=state_var, nomen=nomen_var)
+				think_things_cpy["%s"%(nowlen)] = {"title":title_var, "state":state_var, "nomen":new_res}
+				nowlen+=1
+
+		eval("%s_things"%self.topic).delete(res)
+		eval("%s_things_cpy"%self.topic).pop(res, None)
+		eval("%s_things"%self.topic).put(str(res), category=cat, title=res)
+		self.pop_choices[self.topic][0][res] = cat
+		
+		eval("self.%s()"%self.topic)
+				
+
+	def del_pop(self, pop_item, *args):
+		try:
+			self.popbox.clear_widgets()
+			self.poptop.clear_widgets()
+		except:
+			pass
+		try:
+			self.main_x_box.clear_widgets()
+		except:
+			pass			
+		try:
+			self.popup2.dismiss()
+		except:
+			pass
+		try:
+			self.popup1.dismiss()
+		except:
+			pass			
+		
+		self.pop_bubble.background_color =(20, 0, 0, .5) 
+		self.pop_bubble.border = [50, 50, 50, 10]
+		#self.pop_bubble.size = (150, 50)
+		self.pop_bubble.arrow_pos= 'top_mid'
+		my_bub_lbl=Label(text="Are you sure you want to delete this item?")
+		my_bub_btnY= BubbleButton(text='Yes')
+		my_bub_btnN= BubbleButton(text='No')
+		#my_bub_btn1.bind(on_release=lambda my_bub_btn1: self.Update(1, self.pop_bubble, my_bub_btn1))
+		my_bub_btnY.bind(on_release=lambda my_bub_btnY: self.del_popping(pop_item))
+		#my_bub_btnN.bind(on_release=lambda my_bub_btnN: eval("self.%s()"%self.topic))
+		my_bub_btnN.bind(on_release=lambda my_bub_btnN: self.del_pop_N())
+		#self.pop_bubble.add_widget(my_bub_btnY)
+		#self.pop_bubble.add_widget(my_bub_btnN)
+		#self.add_widget(self.pop_bubble)
+		#self.popbox.add_widget(self.pop_bubble)
+		#self.main_x_box.add_widget(my_bub_lbl)
+		#self.main_x_box.add_widget(my_bub_btnY)
+		#self.main_x_box.add_widget(my_bub_btnN)
+		#self.popup2.open()
+		self.popbox.add_widget(my_bub_lbl)
+		self.popbox.add_widget(my_bub_btnY)
+		self.popbox.add_widget(my_bub_btnN)
+		self.popup1.open()
+
+			
+	def del_popping(self, pop_item, *args):
+
+		try:
+			self.popbox.clear_widgets()
+			self.poptop.clear_widgets()			
+		except:
+			pass
+		try:
+			self.main_x_box.clear_widgets()
+		except:
+			pass			
+		try:
+			self.popup2.dismiss()
+		except:
+			pass
+		try:
+			self.popup1.dismiss()
+		except:
+			pass			
+
+		eval("%s_things"%self.topic).delete(str("%s"%pop_item))
+		self.pop_choices[self.topic][0].pop(pop_item, None)
+		eval("self.%s()"%self.topic)
 
 	main_funcs = {
 	'settings': [],
 	'start': [],
 	#'start': [mindf, state, stati],
-	#'mindf': [prevb, nxtb, pausing],
+	'mindf': [prevb, nxtb, pausing],
 	#'state': [],
 	#'stati': '\n\n'
 	}		
 
 	pop_funcs = {
-	#'mindf' : start_timer,
+	'mindf' : start_timer,
 	#'state' : show_claim,
 	#'stati' : show_ojbs
 	}
@@ -704,7 +1088,7 @@ class gnomieApp(App):
 		return the_screenmanager
 
 	def on_pause(self):
-		return False
+		return True
 
 	def on_resume(self):
 		the_screenmanager = ScreenManager()
@@ -719,6 +1103,9 @@ class gnomieApp(App):
 		mainscreen = MainScreen(name='mainscreen')
 		the_screenmanager.add_widget(mainscreen)
 		return the_screenmanager
+
+	def on_stop(self):
+		pass
 		
 if __name__ == '__main__':
 	gnomieApp().run()
